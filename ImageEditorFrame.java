095a690a229f7278d03602c5c4b1c4a691bd5ff3
Tony Van <tienhuytonny@yahoo.de>
2016-10-24 15:52:39 +0200

    method implemented

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -8,12 +8,35 @@ import javax.imageio.*;
 public class ImageEditorFrame extends JFrame{
 	
 	 public ImageEditorFrame(){
+		 
 		 setDefaultCloseOperation(EXIT_ON_CLOSE);
 		 setSize(400, 300);
 		 setVisible(true);
+		 
 	 }
 	 
 	 public static void main(String[]args){
 		JFrame imageEditorFrame = new ImageEditorFrame();
 	 }
+	 
+	 private void createMenuBar(){
+		 
+		 JMenuBar menuBar = new JMenuBar();
+		 setJMenuBar(menuBar);
+		 JMenu menuFile = new JMenu("File");
+		 menuBar.add(menuFile);
+		 JMenuItem menuItemOpen = new JMenuItem("Open");
+		 menuFile.add(menuItemOpen);
+		 menuItemOpen.addActionListener(
+			 new ActionListener() {
+				 public void actionPerformed(ActionEvent e){
+					 onOpen();
+				}
+			 }
+		 );
+	}
+	private void onOpen(){
+		JOptionPane.showMessageDialog(this, "Open Selected");
+	}
+
 }

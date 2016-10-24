4ce8f0a5c81070fdf3c6981fa13c85a91301c51c
Kevin Hoppe <Kevin Hoppe>
2016-10-24 16:54:45 +0200

    Klasse ImageEditorPanel und Methode

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -6,13 +6,26 @@ import java.io.*;
 import javax.imageio.*;
 
 public class ImageEditorFrame extends JFrame{
+
 	
 	 public ImageEditorFrame(){
 		 setDefaultCloseOperation(EXIT_ON_CLOSE);
 		 setSize(400, 300);
 		 setVisible(true);
-		 setTitle("fance title over 9000");
+		 setTitle("fancy title over 9000");
+		 ImageEditorPanel panel = new ImageEditorPanel();
+		 add(panel);	
 	 }
+
+	 private void setDummyImage(){ 
+	 		BufferedImage bufferedImage = 
+	 			new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB); 
+	 		Graphics g = bufferedImage.getGraphics(); 
+	 		g.setColor(Color.YELLOW); 
+	 		g.fillOval(10, 10, 380, 280); 
+	 		panel.setImage(bufferedImage); 
+	 }
+
 	 
 	 public static void main(String[]args){
 		JFrame imageEditorFrame = new ImageEditorFrame();
